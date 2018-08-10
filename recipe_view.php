df911ff693e2c11b2c88e8385b22add7ad504881
Thomas Chaplin <thomas-chaplin@hotmail.co.uk>
2018-08-11 00:45:40 +0100

    Filled out recipe page with new fields

--- recipe_view.php
+++ recipe_view.php
@@ -12,18 +12,21 @@
     } else {
         $row = mysqli_fetch_assoc($result);
         $title = $row['title'];
-        $recipe_time = $row['recipe_time'];
+        $recipe_active_time = $row['recipe_active_time'];
         $recipe_serves = $row['recipe_serves'];
-        $intro_plntxt = $row['intro_plntxt'];
+        $intro_html = $row['recipe_html'];
+        $ingredients_html = $row['ingredients_html'];
+        $method_html = $row['row_html'];
+        $intro_img = $row['intro_img'];
     }
 ?>
 
 <div class="recipe_intro">
     <div class="recipe_intro_img">
-        <img id="recipe_intro_img">
+        <img src="<?= $intro_img ?>">
     </div>
     <h1><?= $title ?></h1>
-    <p><i class="fas fa-clock" title="Cooking time"></i> <?= $recipe_time ?> &nbsp;&nbsp;
+    <p><i class="fas fa-clock" title="Cooking time"></i> <?= $recipe_active_time ?> &nbsp;&nbsp;
         <i class="fas fa-utensils" title="Serving size"></i> <?= $recipe_serves ?> &nbsp;&nbsp;
         <br id="mobile_linebreak"><br id="mobile_linebreak">
         <a><i class="fab fa-facebook button"title="Share to Facebook"></i></a> &nbsp;&nbsp;
@@ -33,11 +36,17 @@
         <a><i class="fas fa-copy button" title="Copy to clipboard" onclick="copyToClip()"></i></a> &nbsp;&nbsp;
         <a><i class="fas fa-print button"  title="Print recipe"></i></a>
     </p>
-    <p id="recipe_intro"><?= $intro_plntxt ?></p>
+    <p><?= $intro_html ?></p>
 </div>
 <div class="recipe_content_container">
-    <div class="ingredients" id="ingredients"></div>
-    <div class="method" id="method"></div>
+    <div class="ingredients">
+        <h2>Ingredients</h2>
+        <?= $ingredients_html ?>
+    </div>
+    <div class="method">
+        <h2>Method</h2>
+        <?= $method_html ?>
+    </div>
 </div>
 
 
