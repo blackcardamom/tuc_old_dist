0ea325c785d9f27dcf6701138ccf364bcc8081bd
Thomas Chaplin <thomas-chaplin@hotmail.co.uk>
2018-08-16 14:32:47 +0100

    Chagned about page

--- about.php
+++ about.php
@@ -3,15 +3,12 @@
 <div class="about_content">
     <div class="about_img"><img src="assets/portrait2.jpg" alt="Portrait of The Ugly Croissant baker"></div>
     <h2 style="margin:0;padding-top:0.5em">Welcome to The Ugly Crossiant</h2>
-    <p>This website it primarily for all the tastiest recipes I have come up with so far, oh and by the way they have absolutely no eggs! All recipes are vegetarian, and some even vegan. These won't just be substitutes for any recipes you have with eggs,
-        but just delicious recipes that anyone would enjoy.?</p>
-    <h3>A little about myself</h3>
-    <p>I'm a 20 year old student from the UK who has been baking her whole life, I've grown up in a family who all love to cook and so I couldn't help but fall in love with it. However, I didn't get into eggless baking until I met my boyfriend about two
-        years ago; he has an egg allergy. I think the first recipe we made together was chocolate chip cookies, they were simple but can you really beat a cookie? I just wanted to share more food with him, it's not as fun if you have to eat a whole chocolate
-        cake on your own (or is it?). So we experimented with other recipes, as time went on, and eventually The Ugly Croissant was born. The account first started out as posting pictures of experiments I have done, some ended in joy, others in tears.
-        I am still learning massively, and still have no idea what I am doing half of the time, but that's all in the fun of it, but I thought I would share what I have learnt along the way. I have struggled to find lots of eggless recipes online, especially
-        ones that have been tried and tested, and so hopefully this will, somewhat, help fill the gap for everyone. </p>
-    <p>To find all of my posts, experiments and recipe announcements, check out my Instagram account <a href="https://www.instagram.com/theuglycroissant/" style="text-decoration:none;"><i class="fab fa-instagram" style="font-size:1.5em;position:relative;top:0.125em;"></i> @theuglycroissant</a>.
+    <p>This is the home of all of my eggless recipes both sweet and savoury alike. I have designed these to be delicious, whether you choose to eat eggs or not, so please dive straight in.</p>
+    <p><strong>A little about myself</strong></p>
+    <p>My name is Nicola Easton, a 20 year old student from the UK and I am the author of The Ugly Croissant. Baking has been a passion of mine since I was very small and I have grown up testing all of the recipes I can get my hands on, so I thought it was about time that I share some of mine.</p>
+    <p>I started baking eggless recipes about two years ago when I met my boyfriend who has an egg allergy. It’s been an uphill struggle trying to adapt and create recipes, with a few headaches along the way, but so far its been generally successful! This website is here to document my attempts at creating all kinds of delicious goods with a recipe page for all of my best bakes and a blog section on the homepage for comments on slightly less triumphant attempts.</p>
+    <p>The biggest shame I found with eggless cooking is that there are very limited resources online or in books. I want this website to try and fill a gap for those looking for recipes, whether you’re vegetarian, intolerant or just wanting to try some delicious treats.</p>
+    <p>To find all of my posts, experiments and recipe announcements, check out my Instagram account <a href="https://www.instagram.com/theuglycroissant/" style="text-decoration:none;white-space:nowrap;"><i class="fab fa-instagram" style="font-size:1.5em;position:relative;top:0.125em;"></i> @theuglycroissant</a>.
     </p>
     <p><strong>Why the name?</strong></p>
     <p>My dad called my croissants ugly, it caught on.</p>
