9bc9078498d47f9b79a2b7cc2df2a6b806e3d02e
Thomas Chaplin <thomas-chaplin@hotmail.co.uk>
2018-08-29 15:22:38 +0100

    Add address bar colour on mobile

--- header.php
+++ header.php
@@ -29,6 +29,7 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
         <link rel="stylesheet" href="tuc_poc.css">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <meta name="theme-color" content="#48494B"/>
         <title><?= $titlePrefix ?>The Ugly Croissant<?= $titleSuffix ?></title>
 
         <!-- Google meta tags -->
