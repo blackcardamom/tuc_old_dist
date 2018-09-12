eec45aeab17b104061021112f20820993201e364
Thomas Chaplin <thomas-chaplin@hotmail.co.uk>
2018-09-12 16:51:07 +0100

    Changed gallery to auto-recognised pictures

--- gallery.php
+++ gallery.php
@@ -7,17 +7,22 @@
 
 <div class="gallery_wrapper">
     <div class="gallery_selection_window">
-        <img src="<?= $website_root ?>/gallery/Brownies_DSC05549.jpg" onclick="changeDisplay(this)">
-        <img src="<?= $website_root ?>/gallery/DSC05254.JPG" onclick="changeDisplay(this)">
-        <img src="<?= $website_root ?>/gallery/DSC05324.JPG" onclick="changeDisplay(this)">
-        <img src="<?= $website_root ?>/gallery/DSC05349.JPG" onclick="changeDisplay(this)">
-        <img src="<?= $website_root ?>/gallery/DSC05413.JPG" onclick="changeDisplay(this)">
-        <img src="<?= $website_root ?>/gallery/DSC05434.JPG" onclick="changeDisplay(this)">
-        <img src="<?= $website_root ?>/gallery/DSC05460.JPG" onclick="changeDisplay(this)">
-        <img src="<?= $website_root ?>/gallery/Mango%20Tart_DSC05490.jpg" onclick="changeDisplay(this)">
-        <img src="<?= $website_root ?>/gallery/portrait2.jpg" onclick="changeDisplay(this)">
+        <?php
+            $path = $website_root."/gallery/" ;
+            $acceptableExtensions = Array("jpg", "jpeg", "JPG", "png", "PNG");
+
+            $dir = new DirectoryIterator($path);
+            foreach ($dir as $fileinfo) {
+                if (!$fileinfo->isDot() && in_array($fileinfo->getExtension(),$acceptableExtensions)) {
+                    if (!isset($firstFile)) {
+                        $firstFile = $fileinfo->getPathname();
+                    }
+                    echo "<img src='".$fileinfo->getPathname()."' onclick = 'changeDisplay(this)'>";
+                }
+            }
+        ?>
     </div>
-    <div class="gallery_display_port"><img src="<?= $website_root ?>/gallery/Brownies_DSC05549.jpg" id="displayed_pic"></div>
+    <div class="gallery_display_port"><img src="<?= $firstFile ?>" id="displayed_pic"></div>
 </div>
 <script>
     function changeDisplay(elem) {
