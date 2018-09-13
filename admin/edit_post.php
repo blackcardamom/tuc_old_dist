8a22319dbf73904ea09379e50e849255df562508
Thomas Chaplin <thomas-chaplin@hotmail.co.uk>
2018-09-13 18:49:50 +0100

    Post handling pages checking type

--- admin/edit_post.php
+++ admin/edit_post.php
@@ -1,9 +1,21 @@
 <?php
-    include_once 'header.php';
-    $selected = "recipes";
-    include_once 'topnav.php';
+    // Start the session
     session_start();
+
+    // Make sure we're logged in
     if(!isset($_SESSION['uid'])) {
         header("Location: index.php");
         exit;
     }
+
+    // Make sure we have been passed an acceptable type
+    $acceptableTypes = Array("recipe","blogpost");
+    if(!in_array($_GET['type'],$acceptableTypes)) {
+        header("Location: index.php");
+        exit;
+    }
+
+    // Setup the page
+    include_once 'header.php';
+    $selected = $_GET['type']."s";
+    include_once 'topnav.php';
