3998138af5155533804392888d62c8095ed6819c
Thomas Chaplin <thomas-chaplin@hotmail.co.uk>
2018-09-13 13:58:20 +0100

    No change

--- pagenotfound.php
+++ pagenotfound.php
@@ -5,7 +5,7 @@
 ?>
 
 <div style="background-color: white; padding:2%;">
-    <h1 style="text-align:center;padding:0;"> Sorry this page could not be found </h1>
+    <h1 style="text-align:center;padding:0;"> Sorry this page could not be found.</h1>
 </div>
 
 <?php include_once 'footer.php'; ?>
