f89d388879797014678761429272c3a2e37586c5
Thomas Chaplin <thomas-chaplin@hotmail.co.uk>
2018-09-13 17:34:11 +0100

    Styling admin recipe page

--- admin/recipes.php
+++ admin/recipes.php
@@ -10,14 +10,17 @@
     if(!isset($_GET['order'])) {
         $_GET['order'] = 0;
     }
+    if(!isset($_GET['page'])) {
+        $_GET['page'] = 1;
+    }
 ?>
 
 <div class="top_options">
     <div class="top_filters">
         <span>Filter:</span>
         <form action="recipes.php" method="GET">
-            <input type="text" name="title" id="title_input" placeholder="Recipe title...">
-            <input type="text" name="id" id="id_input" placeholder="ID...">
+            <input type="text" name="title" id="title_input" placeholder="Recipe title..."<?= (!empty($_GET['title'])) ? " value='".$_GET['title']."'" : "" ?>>
+            <input type="text" name="id" id="id_input" placeholder="ID..."<?= (!empty($_GET['id'])) ? " value='".$_GET['id']."'" : "" ?>>
             <button type="submit" name="submit"><i class="fas fa-search"></i></button>
         </form>
     </div>
@@ -32,16 +35,93 @@
     </div>
 </div>
 <?php
-    include_once '../includes/pdo.inc.php';
+    include_once 'admin.inc.php';
+    include_once '../includes/Paginator/Paginator.class.php';
+
+    // We figure out the master query
+
+    $master_query = "SELECT * FROM MOCK_TABLE";
+    $searchTitle = !empty($_GET['title']);
+    $searchID = !empty($_GET['id']);
+
+    // Search parameters
+
+    if($searchTitle || $searchID) {
+        $master_query .= " WHERE";
+    }
+    if($searchTitle) {
+        $master_query .= " title LIKE :title";
+    }
+    if($searchTitle && $searchID) {
+        $master_query .= " AND";
+    }
+    if($searchID) {
+        $master_query .= " id = :id";
+    }
+
+    // Order
+    switch ($_GET['order']) {
+        case 0:
+            $master_query .= " ORDER BY date_published DESC";
+            break;
+        case 1:
+            $master_query .= " ORDER BY date_published ASC";
+            break;
+        case 2:
+            $master_query .= " ORDER BY title ASC";
+            break;
+        case 3:
+            $master_query .= " ORDER BY title DESC";
+            break;
+        default:
+            echo "<h1 style='text-align:center; background-color:white; margin:0; padding:30px;'>Sorry we are currently experiencing technical issues.</h1>";
+            exit;
+    }
+
+    // Parameter bind callback
+
+    function recipeSearchCallback($stmt) {
+        if (!empty($_GET['title'])) {
+            $stmt->bindValue('title','%'.$_GET['title'].'%');
+        }
+        if (!empty($_GET['id'])) {
+            $stmt->bindValue('id',$_GET['id']);
+        }
+    }
+
+    // We create our paginator object and update the page
+
+    $paginator = new Paginator($admin_pdo, $master_query, 'recipeSearchCallback');
+    $paginator->updatePage($_GET['page'],20);                                            // Hard coding in 5 results per page
+
 ?>
-<div class="results_table">
+<div class="results_table_wrapper">
     <table>
-        <tr>
+        <tr class="results_table_header">
             <th>ID</th>
             <th>Title</th>
             <th>Actions</th>
         </tr>
+        <?php while($row = $paginator->fetchNextRow()) : ?>
+        <tr>
+            <td><?= $row['id'] ?></td>
+            <td><?= $row['title'] ?></td>
+            <td class="actions_cell"><a href="edit_recipe.php?id=<?= $row['id'] ?>"><i class="fas fa-pencil-alt"></i></a> &nbsp; &nbsp;
+                 <a href="delete_recipe.php?id=<?= $row['id'] ?>"><i class="fas fa-trash-alt"></i></a></td>
+        </tr>
+        <?php endwhile; ?>
     </table>
+</div>
+<div class="pagination_wrapper">
+    <ul class = "horizontal_list pagination_list">
+        <li class="pagination_nav_arrow"><a href="?<?= $paginator->getFirstPageQuery()?>"><i class="fas fa-angle-double-left"></i></a></li>
+        <li class="pagination_nav_arrow"><a href="?<?= $paginator->getPrevPageQuery()?>"><i class="fas fa-angle-left"></i></a></li>
+        <li class="pagination_ellipses">...</li>
+        <?= $paginator->getPagination(5, 'pagination_link', 'current_page'); ?>
+        <li class="pagination_ellipses">...</li>
+        <li class="pagination_nav_arrow"><a href="?<?= $paginator->getNextPageQuery()?>"><i class="fas fa-angle-right"></i></a></li>
+        <li class="pagination_nav_arrow"><a href="?<?= $paginator->getLastPageQuery()?>"><i class="fas fa-angle-double-right"></i></a></li>
+    </ul>
 </div>
 
 <script>
