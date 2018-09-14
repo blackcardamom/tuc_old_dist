7945632ea2817bba1181bbfd965827502e3a8853
Thomas Chaplin <thomas-chaplin@hotmail.co.uk>
2018-09-14 13:01:47 +0100

    Added logout button

--- admin/users.php
+++ admin/users.php
@@ -8,6 +8,15 @@
         exit;
     }
     print_r($_SESSION);
+    if(isset($_POST['logout'])) {
+        session_destroy();
+        header("Location: index.php");
+        exit;
+    }
 ?>
+
+<form action="users.php" method="post">
+    <button name="logout" type="submit" class="action_button">Logout</button>
+</form>
 
 <?php include_once 'footer.php'; ?>
