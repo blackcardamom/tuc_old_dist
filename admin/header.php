538f7302d2a927f7c2a8c5089898b03d9a9c5e0d
Thomas Chaplin <thomas-chaplin@hotmail.co.uk>
2018-09-22 15:45:44 +0100

    Added jQuery UI to header

--- admin/header.php
+++ admin/header.php
@@ -7,7 +7,18 @@
     <meta name="robots" content="noindex">
     <link rel="stylesheet" href="admin.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <?php if(isset($useJqueryUI) && $useJqueryUI ): ?>
+        <link rel="stylesheet" href="../includes/jquery-ui.min.css">
+    <?php endif; ?>
     <title>The Ugly Croissant - Admin</title>
 </head>
+
+<?php if(isset($useJqueryUI) && $useJqueryUI ): ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="../includes/jquery-ui.min.js"></script>
+    <!--script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script-->
+    <script src="../includes/jquery.ui.touch-punch.min.js"></script>
+<?php endif; ?>
+
 <?php if(!isset($loginBody)) { $loginBody = false; } ?>
 <body id = "<?= $loginBody ? 'loginBody' : 'adminBody' ?>">
